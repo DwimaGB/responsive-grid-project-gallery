e1f184cfe32dee817b64697a8b4f878cc4de0231
Dwima Basumatary <dwima1583@gmail.com>
2023-03-29 11:13:39 +0530

    final commit

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+
+const navToggleBtn =  document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggleBtn.addEventListener('click', ()=>{
+    const isVisible = primaryNav.getAttribute('data-visible');
+    console.log(isVisible);
+
+    if(isVisible == 'true'){
+        primaryNav.setAttribute('data-visible', 'false');
+        navToggleBtn.setAttribute('aria-expanded', 'false');
+    }
+    else{
+        primaryNav.setAttribute('data-visible', 'true');
+        navToggleBtn.setAttribute('aria-expanded', 'true');
+    }
+})
